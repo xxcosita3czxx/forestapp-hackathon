1e31dafe22021bb2d357d97ecdf921e694cfca90
huewhg <adam.dragon.karasek@gmail.com>
2024-12-06 17:43:12 +0100

    ws chat mgr

--- Backend/api/ws/ws.py
+++ Backend/api/ws/ws.py
@@ -6,7 +6,14 @@ app = fastapi.FastAPI()
 
 # Create routr
 router = fastapi.APIRouter()
-
+def extract(name, i):
+    out = ""
+    if name in i:
+        i = i.replace(name,"")
+        for y in i:
+            if y != "\"":
+                out += y
+    return out
 @router.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
@@ -18,12 +25,18 @@ async def websocket_endpoint(websocket: WebSocket):
                 content = ""
                 recipientid = ""
                 senderid = ""
-                data = data.replace("[message]")
-
-                print(data)
-                    #for i in data:
-                        
-            
+                data = data.replace("[message]", "")
+                lsdata = data.split(",")
+                for i in lsdata:
+                    if "conent" in i:
+                        content = extract("content:", i)
+                    if "recipientid" in i:
+                        recipientid = extract("recipientid:", i)
+                    if "senderid" in i:
+                        senderid = extract("senderid:", i)
+                print(content)
+                print(recipientid)
+                print(senderid)
         else:
             await websocket.send_text("Malformed request")
 # todle tu musi bejt nesahej na to
