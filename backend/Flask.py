0a5b4a051f6ff91f4e45135fd650c8008381a5d4
huewhg <adam.dragon.karasek@gmail.com>
2024-12-06 13:51:07 +0100

    flask cry

--- backend/Flask.py
+++ backend/Flask.py
@@ -15,20 +15,9 @@ def index():
     context = {
             "test":testf,
         }
-    resp = make_response(render_template("day.html", **context))
+    resp = make_response(render_template("index.html", **context))
     return resp
-def main(debug,cert,key,port=5000,ip="127.0.0.1"):
-    if debug:
-        if cert and key:
-            context = (cert, key)
-            app.run(debug=True,port=port,host=ip,ssl_context=context)  # noqa: S201
-        else:
-            app.run(debug=True,port=port,host=ip)  # noqa: S201
-    else:
-        if cert and key:
-            context = (cert, key)
-            app.run(port=port,host=ip,ssl_context=context)
-        else:
-            app.run(port=port,host=ip)
+def main(port=5000,ip="127.0.0.1"):
+    app.run(port=port,host=ip)
 if __name__ == '__main__':
     main()
