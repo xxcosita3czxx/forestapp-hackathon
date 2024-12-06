43c9b9830cf3f89659d9e5ee654ba1713aa33ef3
xxcosita3czxx <kokosove18@gmail.com>
2024-12-06 16:36:47 +0100

    ws fix v343823432525235

--- Backend/app.py
+++ Backend/app.py
@@ -3,7 +3,7 @@ import importlib.util
 import os
 import sys
 
-from fastapi import APIRouter, FastAPI, WebSocket
+from fastapi import APIRouter, FastAPI
 
 app = FastAPI()
 
@@ -15,32 +15,30 @@ current_script_directory = os.path.dirname(os.path.abspath(sys.argv[0]))
 scripts_directory = os.path.join(current_script_directory)
 os.chdir(scripts_directory)
 
-# Function to dynamically load routes
+# Function to dynamically load routes from a directory
 def load_routes_from_directory(directory, parent_router=None):
     for filename in os.listdir(directory):
         full_path = os.path.join(directory, filename)
 
         if os.path.isdir(full_path):
-            # If the directory, recursively call load_routes_from_directory
+            # If it's a directory, recursively process its contents
             sub_router = APIRouter()
             load_routes_from_directory(full_path, sub_router)
+            # Include the sub-router in the parent router or directly in the app
             if parent_router is not None:
                 parent_router.include_router(sub_router, prefix=f"/{filename}")
             else:
                 app.include_router(sub_router, prefix=f"/{filename}")
 
         elif filename.endswith('.py') and filename != '__init__.py':
-            module_name = filename[:-3]  # Strip '.py'
+            # Import the Python module dynamically
+            module_name = filename[:-3]  # Strip '.py' from filename
             module_path = full_path
             spec = importlib.util.spec_from_file_location(module_name, module_path)
             module = importlib.util.module_from_spec(spec)
             spec.loader.exec_module(module)
 
-            if hasattr(module, 'websocket'):
-                route_path = f"/{module_name}"  # Dynamically create the route path
-                app.websocket(route_path)(module.websocket)
-
-            # Assuming each module has an APIRouter or endpoint function to add
+            # Check if the module has a router (WebSocket or API routes) and include it  # noqa: E501
             if hasattr(module, 'router'):
                 if parent_router is not None:
                     parent_router.include_router(module.router)
