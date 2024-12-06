e9d63646bcf1f001e6c0a336ded634dab050f5d3
xxcosita3czxx <kokosove18@gmail.com>
2024-12-06 17:16:56 +0100

    some more additions

--- Backend/api/users/add.py
+++ Backend/api/users/add.py
@@ -1,8 +1,11 @@
 import fastapi
 import utils.configmanager as cm
+
 router = fastapi.APIRouter()
 
 @router.get("/add")
 def add_user():
     try:
-        cm.use
+        cm.users.set()
+    except Exception:
+        return "error"
