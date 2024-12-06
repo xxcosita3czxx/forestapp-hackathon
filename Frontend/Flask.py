4435d21da4cd3e7acad591c7a34883be94831ac7
huewhg <adam.dragon.karasek@gmail.com>
2024-12-06 13:29:17 +0100

    flask setup

--- Frontend/Flask.py
+++ Frontend/Flask.py
@@ -1,6 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template, make_response, request, jsonify
+import os
+import requests
+from config import Config
 app = Flask(
     __name__,
     template_folder=os.path.abspath(Config.TEMPLATE_FOLDER),
-    static_folder=os.path.abspath(Config.STATIC_FOLDER)
+    static_folder=os.path.abspath(Config.STATIC_FOLDER),
 )
+app.config.from_object(Config)
