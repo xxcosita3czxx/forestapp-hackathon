ebe59aafca35f259c9aa45109f3dfa0e2d8545a4
xxcosita3czxx <kokosove18@gmail.com>
2024-12-07 13:32:25 +0100

    upload

--- Backend/api/auth/verify.py
+++ Backend/api/auth/verify.py
@@ -3,6 +3,6 @@ from fastapi import HTTPException
 
 router = fastapi.APIRouter()
 
-@router.get("/verify")
-def verify():
+@router.post("/verify/{sessionid}")
+def verify(sessionid:str):
     return HTTPException(status_code=200,detail="Success")
