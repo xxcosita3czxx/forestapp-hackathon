6df527cd11d9ccd37ae9810efa58fecf20187349
xxcosita3czxx <kokosove18@gmail.com>
2024-12-07 20:55:31 +0100

    new

--- Backend/api/forum/posts/create.py
+++ Backend/api/forum/posts/create.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import fastapi
 import utils.configmanager as cm
 
@@ -7,11 +9,8 @@ router = fastapi.APIRouter()
 def create_post(id:str,post_id:str,title:str,text:str,author_id:str):
     cm.forums.set(id,post_id,"title",title)
     cm.forums.set(id,post_id,"text",text)
+    cm.forums.set(datetime.now())
     cm.forums.set(id,post_id,"author_id",author_id)
+    cm.forums.set(id,post_id,"likes","0")
+    cm.forums.set(id,post_id,"comments","{}")
     return fastapi.HTTPException(status_code=200,detail="Success")
-
-[post_id]
-title = "title"
-text = "text"
-author_id = "author_id"
-comments ="{}"
--- Backend/api/users/settings/set.py
+++ Backend/api/users/settings/set.py
@@ -1,7 +1,9 @@
 import fastapi
+import utils.configmanager as cm
 
 router = fastapi.APIRouter()
 
-@router.post("/set")
-def set_settings():
-    return {"message": "Settings set"}
+@router.patch("/set/{setting}")
+def set_settings(setting:str=fastapi.Depends(None)):
+    settings = cm.sessions.config.get("default_settings")
+    return settings
