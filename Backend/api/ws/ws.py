220d3e87ad2540b38c3e6de4b0def4c8b0f291b3
huewhg <adam.dragon.karasek@gmail.com>
2024-12-07 14:37:03 +0100

    fixed new user bug

--- Backend/api/ws/ws.py
+++ Backend/api/ws/ws.py
@@ -16,14 +16,14 @@ async def websocket_endpoint(websocket: WebSocket):
         data = json.loads(data)
         if data["type"] == "message":
             contacts = cm.users.get(data["recipientid"], "general", "contacts")
-            if contacts is None:
+            if cm.users.get(data["recipientid"], "general", "name") is not None and contacts is None:
                 contacts = data["senderid"]
-                cm.users.set(data["recipientid"], "general", "contacts", contacts)   
-            if data["senderid"] not in contacts:
-                contacts += f",{data["senderid"]}"
                 cm.users.set(data["recipientid"], "general", "contacts", contacts)
+                if data["senderid"] not in contacts:
+                    contacts += f",{data["senderid"]}"
+                    cm.users.set(data["recipientid"], "general", "contacts", contacts)
             contacts = cm.users.get(data["senderid"], "general", "contacts")
-            if contacts is None:
+            if cm.users.get(data["senderid"], "general", "name") is not None and contacts is None:
                 contacts = data["recipientid"]
                 cm.users.set(data["senderid"], "general", "contacts", contacts)
             if data["recipientid"] not in contacts:
