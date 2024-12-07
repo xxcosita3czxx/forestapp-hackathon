996185e80ffef2c2c9d97df21652eabc3429ddd5
xxcosita3czxx <kokosove18@gmail.com>
2024-12-07 21:19:18 +0100

    swe

--- Backend/api/users/settings/set.py
+++ Backend/api/users/settings/set.py
@@ -3,7 +3,11 @@ import utils.configmanager as cm
 
 router = fastapi.APIRouter()
 
-@router.patch("/set/{setting}")
-def set_settings(setting:str=fastapi.Depends(None)):
-    settings = cm.sessions.config.get("default_settings")
+@router.patch("/set/{user_id}&{title}&{setting}&{key}")
+def patch(user_id:str,title:str,setting:str,key:str):
+    settings = cm.users.set(user_id,title,setting,key)
     return settings
+@router.get("/set/{user_id}")
+def get(user_id:str):
+    settings = cm.users.config.get(user_id)
+    return settings()
