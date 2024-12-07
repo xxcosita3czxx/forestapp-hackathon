c51e1de3dcf8ab5bccea00d7ba8121419fed354e
xxcosita3czxx <kokosove18@gmail.com>
2024-12-07 21:58:03 +0100

    lowerlower

--- Backend/api/forum/category/create.py
+++ Backend/api/forum/category/create.py
@@ -5,6 +5,6 @@ router = fastapi.APIRouter()
 
 @router.post("/create")
 def create_cat(id:str,title:str,desc:str):
-    cm.forums.set(id,"general","title",title)
-    cm.forums.set(id,"general","description",desc)
+    cm.forums.set(id.lower(),"general","title",title)
+    cm.forums.set(id.lower(),"general","description",desc)
     return fastapi.HTTPException(status_code=200,detail="Success")
