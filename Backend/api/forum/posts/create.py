53cb421d6072d84e6dd5356f2daff39fbfcaa523
xxcosita3czxx <kokosove18@gmail.com>
2024-12-07 20:44:39 +0100

    fetc

--- Backend/api/forum/posts/create.py
+++ Backend/api/forum/posts/create.py
@@ -4,7 +4,14 @@ import utils.configmanager as cm
 router = fastapi.APIRouter()
 
 @router.post("/create")
-def create_post(id:str,title:str,text:str,author_id:str):
-    cm.forums.set(id,title,"text",text)
-    cm.forums.set(id,title,"author_id",author_id)
+def create_post(id:str,post_id:str,title:str,text:str,author_id:str):
+    cm.forums.set(id,post_id,"title",title)
+    cm.forums.set(id,post_id,"text",text)
+    cm.forums.set(id,post_id,"author_id",author_id)
     return fastapi.HTTPException(status_code=200,detail="Success")
+
+[post_id]
+title = "title"
+text = "text"
+author_id = "author_id"
+comments ="{}"
