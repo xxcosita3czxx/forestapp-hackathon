0a77e28f8c717dbe6701877749606f44d9a36cc6
xxcosita3czxx <kokosove18@gmail.com>
2024-12-07 16:55:51 +0100

    categories

--- Backend/api/forum/posts/create.py
+++ Backend/api/forum/posts/create.py
@@ -4,5 +4,6 @@ import utils.configmanager as cm
 router = fastapi.APIRouter()
 
 @router.post("/create")
-def create_post():
-    return 200
+def create_post(id:str,title:str,text:str):
+    cm.forums.set(id,title,"text",text)
+    return fastapi.HTTPException(status_code=200,detail="Success")
