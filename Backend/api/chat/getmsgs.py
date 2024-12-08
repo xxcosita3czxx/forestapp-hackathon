ec1746ff0a3c8b8058925461c02a7f1dfaf1e3d8
xxcosita3czxx <kokosove18@gmail.com>
2024-12-08 01:26:32 +0100

    fix

--- Backend/api/chat/getmsgs.py
+++ Backend/api/chat/getmsgs.py
@@ -1,9 +1,10 @@
+import api.auth.verify_pass as vpass
 import fastapi
 import utils.configmanager as cm
 
 router = fastapi.APIRouter()
 @router.get("/fetchconvos/{uuid}")
-def fetch(uuid:str,login:bool=False):
+def fetch(uuid:str,login:bool=False,authorize:str=fastapi.Depends(vpass.verify_permission_un)):  # noqa: E501
     users = cm.users.config
     print(users[uuid])
     return users[uuid]["messages"]
