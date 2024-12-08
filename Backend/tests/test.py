a72b62d763b6d0bf5ce25728c506907f7af34fa3
xxcosita3czxx <kokosove18@gmail.com>
2024-12-08 12:19:01 +0100

    fix pass check

---
+++ Backend/tests/test.py
@@ -0,0 +1,4 @@
+import api.users.add as add  # noqa: F401
+import utils.configmanager as cm  # noqa: F401
+
+#print(add.check_aes_code("mrnobody3.",cm.users.get("dc14774f-f8e2-4246-8cbe-7c71533d8384","general","pass"),"success-dc14774f-f8e2-4246-8cbe-7c71533d8384"))
