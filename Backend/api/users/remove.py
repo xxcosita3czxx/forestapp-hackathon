335ffda340dc0958679cff020256d51df5df3e74
xxcosita3czxx <kokosove18@gmail.com>
2024-12-08 01:54:19 +0100

    gitlab remove

--- Backend/api/users/remove.py
+++ Backend/api/users/remove.py
@@ -1,3 +1,4 @@
+import api.auth.verify_pass as vpass
 import api.users.fetch as fetch
 import fastapi
 import utils.configmanager as cm
@@ -5,7 +6,7 @@ import utils.configmanager as cm
 router = fastapi.APIRouter()
 
 @router.delete("/remove/{user}")
-def remove_user(user:str):
+def remove_user(user:str,authorization:str=fastapi.Depends(vpass.verify_permission_diez)):  # noqa: E501
     fetch.fetch(user,full_match=True)
     cm.users.delete()
     return {"message": "User removed"}
