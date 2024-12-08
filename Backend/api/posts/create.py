ceaf765aabbe174c1e8cc3a45618d8bb3793fe65
StaiMerr <107706569+StaiMerr@users.noreply.github.com>
2024-12-08 09:51:24 +0100

    Reapply "Merge branch 'main' of https://github.com/xxcosita3czxx/forestapp-hackathon"
    This reverts commit edd601e9aed415a47c9076f8112deb8b6a3bca55.

--- Backend/api/posts/create.py
+++ Backend/api/posts/create.py
@@ -5,5 +5,7 @@ import utils.configmanager as cm
 router = fastapi.APIRouter()
 
 @router.post("/create")
-def create_post(id:str,authorization:str=fastapi.Depends(vpass.verify_permission_diez)):  # noqa: E501
+def create_post(id:str,title:str,description:str,authorization:str=fastapi.Depends(vpass.verify_permission_diez)):  # noqa: E501
+    cm.posts.set(id,"general","title",title)
+    cm.posts.set(id,"general","description",description)
     return fastapi.HTTPException(status_code=200,detail="Success")
--- Backend/api/posts/fetchall.py
+++ Backend/api/posts/fetchall.py
@@ -5,5 +5,5 @@ import utils.configmanager as cm
 router = fastapi.APIRouter()
 
 @router.get("/fetchall")
-def create_post(id:str,authorization:str=fastapi.Depends(vpass.verify_permission_diez)):  # noqa: E501
-    return fastapi.HTTPException(status_code=200,detail="Success")
+def create_post(authorization:str=fastapi.Depends(vpass.verify_permission_diez)):  # noqa: E501
+    return cm.posts.config
